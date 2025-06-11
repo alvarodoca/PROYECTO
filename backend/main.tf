f57bc38a07e9650a5c876e2afabb3c8e54a0b564
alvarodoca <adomcas163@g.educaand.es>
2025-06-11 17:23:50 +0200

    Update main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -55,77 +55,81 @@ resource "aws_instance" "ftp_instancia" {
   }
 
 
-  user_data = <<-EOF
+    user_data = <<-EOF
     #!/bin/bash
-    sudo apt-get update -y
-    sudo apt-get install -y ldap-utils docker.io s3fs
+    # Actualizar el repositorio e instalar Docker y S3FS
+    sudo apt-get update
+    sudo apt-get install -y ldap-utils
+    sudo apt-get install -y docker.io s3fs
+    
 
+    # Iniciar servicio Docker
     sudo systemctl start docker
     sudo systemctl enable docker
 
+    # Crear Dockerfile para ProFTP
     mkdir -p /home/admin/proftp
     cat << 'EOT' > /home/admin/proftp/Dockerfile
     FROM debian:latest
-    RUN apt-get update && apt-get install -y proftpd openssl nano ldap-utils
-    RUN apt-get install -y proftpd-mod-crypto proftpd-mod-ldap
-
+    RUN apt-get update && apt-get install -y proftpd openssl nano
+    RUN apt-get install -y proftpd-mod-crypto
+   
+    # Generar el certificado SSL/TLS para ProFTPD
     RUN openssl req -x509 -newkey rsa:2048 -sha256 -keyout /etc/ssl/private/proftpd.key -out /etc/ssl/certs/proftpd.crt -nodes -days 365 -subj "/C=ES/ST=España/L=Atarfe/O=alvaro/OU=alvaro/CN=ftp.alvaro.com"
 
-    RUN adduser --disabled-password --gecos "" alvaro
-    RUN echo "alvaro:alvaro" | chpasswd
+
+    # Configuración de ProFTPD
+    RUN echo "Include /etc/proftpd/tls.conf" >> /etc/proftpd/proftpd.conf && \
+    echo "LoadModule mod_ctrls_admin.c" >> /etc/proftpd/modules.conf && \
+    echo "<IfModule mod_tls.c>" >> /etc/proftpd/tls.conf && \
+    echo "  TLSEngine on" >> /etc/proftpd/tls.conf && \
+    echo "  TLSLog /var/log/proftpd/tls.log" >> /etc/proftpd/tls.conf && \
+    echo "  TLSProtocol SSLv23" >> /etc/proftpd/tls.conf && \
+    echo "  TLSRSACertificateFile /etc/ssl/certs/proftpd.crt" >> /etc/proftpd/tls.conf && \
+    echo "  TLSRSACertificateKeyFile /etc/ssl/private/proftpd.key" >> /etc/proftpd/tls.conf && \
+    echo "</IfModule>" >> /etc/proftpd/tls.conf && \
+    echo "LoadModule mod_tls.c" >> /etc/proftpd/modules.conf
     
-    RUN sed -i '/<IfModule mod_quotatab.c>/,/<\/IfModule>/d' /etc/proftpd/proftpd.conf
-    RUN echo "LoadModule mod_tls.c" >> /etc/proftpd/modules.conf
-    RUN echo "LoadModule mod_ctrls_admin.c" >> /etc/proftpd/modules.conf
-    RUN echo "LoadModule mod_ldap.c" >> /etc/proftpd/modules.conf
-    RUN echo "<IfModule mod_tls.c>" > /etc/proftpd/tls.conf
-    RUN echo "  TLSEngine on" >> /etc/proftpd/tls.conf
-    RUN echo "  TLSLog /var/log/proftpd/tls.log" >> /etc/proftpd/tls.conf
-    RUN echo "  TLSProtocol SSLv23" >> /etc/proftpd/tls.conf
-    RUN echo "  TLSRSACertificateFile /etc/ssl/certs/proftpd.crt" >> /etc/proftpd/tls.conf
-    RUN echo "  TLSRSACertificateKeyFile /etc/ssl/private/proftpd.key" >> /etc/proftpd/tls.conf
-    RUN echo "</IfModule>" >> /etc/proftpd/tls.conf
-    RUN mkdir -p /etc/proftpd/ssl
-    
-    RUN echo "Include /etc/proftpd/tls.conf" >> /etc/proftpd/proftpd.conf
-    
-    RUN mkdir /home/ftp
-    RUN chmod 777 -R /home/ftp
-    RUN echo "DefaultRoot /home/ftp" >> /etc/proftpd/proftpd.conf
-    RUN echo "PassivePorts 1024 1048" >> /etc/proftpd/proftpd.conf
-    RUN echo "<Anonymous /home/ftp>" >> /etc/proftpd/proftpd.conf
-    RUN echo "  User ftp" >> /etc/proftpd/proftpd.conf
-    RUN echo "  Group nogroup" >> /etc/proftpd/proftpd.conf
-    RUN echo "  UserAlias anonymous ftp" >> /etc/proftpd/proftpd.conf
-    RUN echo "  RequireValidShell off" >> /etc/proftpd/proftpd.conf
-    RUN echo "  MaxClients 10" >> /etc/proftpd/proftpd.conf
-    RUN echo "  <Directory *>" >> /etc/proftpd/proftpd.conf
-    RUN echo "    <Limit WRITE>" >> /etc/proftpd/proftpd.conf
-    RUN echo "      DenyAll" >> /etc/proftpd/proftpd.conf
-    RUN echo "    </Limit>" >> /etc/proftpd/proftpd.conf
-    RUN echo "  </Directory>" >> /etc/proftpd/proftpd.conf
-    RUN echo "</Anonymous>" >> /etc/proftpd/proftpd.conf
-    
-    
-    
-    
-    CMD ["proftpd", "--nodaemon", "--debug", "10"]
+    RUN echo "DefaultRoot /home/alvaro" >> /etc/proftpd/proftpd.conf && \
+    echo "PassivePorts 1024 1048" >> /etc/proftpd/proftpd.conf && \
+    echo "Port 21" >> /etc/proftpd/proftpd.conf && \
+    echo "Port 20" >> /etc/proftpd/proftpd.conf && \
+    echo "<Anonymous /home/alvaro>" >> /etc/proftpd/proftpd.conf && \
+    echo "  RequireValidShell off" >> /etc/proftpd/proftpd.conf && \
+    echo "  User ftp" >> /etc/proftpd/proftpd.conf && \
+    echo "  Group nogroup" >> /etc/proftpd/proftpd.conf && \
+    echo "  UserAlias anonymous ftp" >> /etc/proftpd/proftpd.conf && \
+    echo "  <Limit WRITE>" >> /etc/proftpd/proftpd.conf && \
+    echo "    DenyAll" >> /etc/proftpd/proftpd.conf && \
+    echo "  </Limit>" >> /etc/proftpd/proftpd.conf && \
+    echo "  <Limit READ>" >> /etc/proftpd/proftpd.conf && \
+    echo "    AllowAll" >> /etc/proftpd/proftpd.conf && \
+    echo "  </Limit>" >> /etc/proftpd/proftpd.conf && \
+    echo "</Anonymous>" >> /etc/proftpd/proftpd.conf
+
+
+    # Crear usuario FTP
+    RUN adduser --disabled-password --gecos "" alvaro && \
+    echo "alvaro:alvaro" | chpasswd
+ 
+    CMD ["proftpd", "--nodaemon"]
     EXPOSE 20 21 1024-1048
+
     EOT
 
     # Crear archivo de credenciales de AWS
     sudo mkdir -p ~/.aws
     cat > ~/.aws/credentials <<-AWS_CREDENTIALS
     [default]
-    aws_access_key_id=ASIAYS2NW4H6FHRBILTD
-    aws_secret_access_key=wkMYeqoyQ8t5dw2CgIEiuodB/+pWlFWjoileFghT
-    aws_session_token=IQoJb3JpZ2luX2VjEAgaCXVzLXdlc3QtMiJIMEYCIQD/CxfpZR/K16ZT6ThkhmP4lSVxbY6+54xnGNDdlI5UBAIhAKx+AuiJ7p0X4OvgfauAaQFNSA95waorxV6sP7e/RORqKrwCCMH//////////wEQABoMNTkwMTg0MTEyNjM2IgyZwYkIC6z6R9CD7bwqkAIqM/yqPROFi7oxVoxB9QcBhdYmaZDqCH0EmIOS1es/9cscP/Z9YOxsaFiBpLQhaOlnAT9p4xwIj02UWmeOHpC+pXZ37LZ/B43g6Jk1MPCh6xp3vv/uRZShnhGc1KcvNWYpvI8WloP1XKYrt14oINt0NiEzTcLHMnsOCWC8ssEyt+98AHOq8YJjwHWUHbFqDjD0wvXMz9TzPSbcBWXqxnZKPKcJenLd0Kq7zM3VErujO/kGdn7AtRjGuB0v33+7iNTdwZ22EVxrKUs1GMrEO42Vz/prk4c7OW0grMdJUGL0gvssnj4s2qu7DhZ6P9WImloWvF7l+XPKsYi+jU/uwYttxhqvy/fbcSooBRNjLyQYhzCO5rfBBjqcAZIjsGTJrWlVsTSzkGGnGsOyoqKs1RVZim25qtjAIzUYTnApSZ2M55RjPK/j2pV/EcBaqEUMdyyvek2QqRKPVW7ZUxruoE+9svVC0CuGEYIItqQlVKMOnZm4aLJTeFeV128hHGW/xIxyTtW5FmeWVdUEmXXHKBE7fTBTbC2BO0B6Uo8lOiGmVvVTtFFNqIo9PPUJvb5XG8FbAgZ6KQ==
+    aws_access_key_id=ASIAYS2NW4H6L24TS6JU
+    aws_secret_access_key=nYSl3fMDlq54V9+pNgUuK4mL4cwOXt76Ud9I6iIv
+    aws_session_token=IQoJb3JpZ2luX2VjENP//////////wEaCXVzLXdlc3QtMiJGMEQCIBCNubxoGpfi8UeQLr+xcTHwsZMSTBmXdYhGZT1B2Rh5AiBck6hSrni4VXnDwS+J8RwXuZvxQPe3PnOwiLPDLCdNliq8Agis//////////8BEAAaDDU5MDE4NDExMjYzNiIMQ67iPFjzicsNCYNiKpACda6mUEUkiC9vvJbAX1MQENK4S7EuT/+3m+5mxQp19p2egwRlJM8/mFxEubpgt3U7LUv3yh1jdBJempLcdAmlPuqq2rDgh0Pd1Sy1vY19EOGizcwP/Me2eE5YAY3FVPVug9qbaCwLDQM6VQVUn1H3V/a1dym8Mvm8Xovva64pwyMbdayt0oHzn12bAQGW+VDwiPUF3wlXTn8z0e7upqIe0prPDsqZZQlYT8VQtDraDqSnFgFqoo+IpoQjsBbGmecFW3HHqfZErjIXztb4ZINpodh9kiKdwoB3OwyG0SpNQfvOGXWgIVs81qSXS18e5+OndDsA5co7amrrZOD5twbtxxKNrFQOcjyuzfXg43+9wGYwsdOcwgY6ngEs4ZDSIth6NIaDvDs4qDXxM5V41eAsP+8R6u2d6jgeJFEBYcwGrrTyYP0cEwwdt5/AMKvq17OASU2LgcGTFFyroN+08SnJcOUXklHmbLM1CLctKGLKe79Acc418AsjSu0B8SYFGqQpp89CAGVo1lv89hFNk6yqrFZeDE3bXA3dQDw9jfdbUhDLaTOIboY1RIZ7nLvrF7ME12H7mnP1EA==
     AWS_CREDENTIALS
 
     # Crear directorio del bucket de S3
     sudo mkdir -p /home/admin/carpeta_bucket
     sudo chmod 755 /home/admin/carpeta_bucket
-
+    
     # Montar el bucket S3
     sudo s3fs proyecto-alvaro /home/admin/carpeta_bucket -o allow_other
 
@@ -133,5 +137,6 @@ resource "aws_instance" "ftp_instancia" {
     cd /home/admin/proftp
     sudo docker build -t proftp .
     sudo docker run -d --name proftp -p 21:21 -p 20:20 -p 1024-1048:1024-1048 -v /home/admin/carpeta_bucket:/home/alvaro proftp
-EOF
+  EOF
+
 }
